8b3741acb881288a3cdeff7a60f359bdf3f86af3
wayasmiguel <wayasmiguel@outlook.com>
2020-02-09 23:01:40 -0600

    Conexión con Atlas

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ var mongoose = require('mongoose');
 var app = require('./app');
 var port = 3900;
 
+//'mongodb+srv://wayasMongo:MongoDBNode@nodebackendcluster-uh5h0.mongodb.net/db_starwars?retryWrites=true&w=majority'
+//'mongodb://localhost:27017/db_starwars'
+
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/db_starwars', { useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect('mongodb+srv://wayasMongo:MongoDBNode@nodebackendcluster-uh5h0.mongodb.net/db_starwars?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
     console.log('La conexión a la base de datos se ha realizado correctamente!');
 
